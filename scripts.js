ca39281cc5d235ef11166658db75d49611341cdb
lucasmessias91 <lmessias91@gmail.com>
2025-04-15 19:39:05 -0300

    Primeiro Commit - Projeto Nike

---
+++ scripts.js
@@ -0,0 +1,6 @@
+let body = document.querySelector("body")
+let tenis = document.querySelector("#tenis")
+function mudarVisual(cor, img) {
+    body.style.backgroundColor = cor
+    tenis.src = img
+}
